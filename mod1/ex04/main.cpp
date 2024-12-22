86f22d424fa01f7ca1c1546f22e7028127baec19
Anttoni Maula <manttoni92@gmail.com>
2024-12-22 13:53:26 +0200

    error handlind

--- mod1/ex04/main.cpp
+++ mod1/ex04/main.cpp
@@ -22,7 +22,18 @@ int main(int argc, char **argv)
 	}
 
 	std::ifstream original_file(argv[1]);
+		if (!original_file.is_open())
+	{
+		std::cerr << "Error opening file: " << argv[1] << std::endl;
+		return (2);
+	}
+
 	std::ofstream replace_file(std::string(argv[1]) + ".replace");
+	if (!replace_file.is_open())
+	{
+		std::cerr << "Error creating file: " << argv[1] << ".replacement" << std::endl;
+		return (3);
+	}
 
 	std::string line;
 	std::string to_replace = (std::string)argv[2];
@@ -33,7 +44,9 @@ int main(int argc, char **argv)
 		ft_replace(line, to_replace, replacement);
 		replace_file << line << std::endl;
 	}
+
 	original_file.close();
 	replace_file.close();
+	
 	return (0);
 }
