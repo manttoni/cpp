16e278130c9db889b883c0e3eb674ba562b5c376
Anttoni Maula <amaula@c2r5p8.hive.fi>
2025-01-12 16:31:59 +0200

    no empty to_replace allowed, new way to read from file

--- mod1/ex04/main.cpp
+++ mod1/ex04/main.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <sstream>
 
 static void ft_replace(std::string &line, std::string &to_replace, std::string &replacement)
 {
@@ -17,15 +18,21 @@ int main(int argc, char **argv)
 {
 	if (argc != 4)
 	{
-		std::cout << "Wrong amount of arguments. Wanted <filename> <to replace> <replacement>" << std::endl;
+		std::cout << "Wrong amount of arguments. Wanted <filename> <to_replace> <replacement>" << std::endl;
 		return (1);
+	}
+
+	if (std::string(argv[2]).empty())
+	{
+		std::cout << "to_replace can't be empty." << std::endl;
+		return (2);
 	}
 
 	std::ifstream original_file(argv[1]);
 	if (!original_file.is_open())
 	{
 		std::cerr << "Error opening file: " << argv[1] << std::endl;
-		return (2);
+		return (3);
 	}
 
 	std::ofstream replace_file(std::string(argv[1]) + ".replace");
@@ -33,20 +40,17 @@ int main(int argc, char **argv)
 	{
 		std::cerr << "Error creating file: " << argv[1] << ".replace" << std::endl;
 		original_file.close();
-		return (3);
+		return (4);
 	}
 
-	std::string line = "\n";
 	std::string to_replace = (std::string)argv[2];
 	std::string replacement = (std::string)argv[3];
+	std::stringstream s;
+	s << original_file.rdbuf();
+	std::string text = s.str();
 
-	while (getline(original_file, line))
-	{
-		if (line != "\n")
-			replace_file << std::endl;
-		ft_replace(line, to_replace, replacement);
-		replace_file << line;
-	}
+	ft_replace(text, to_replace, replacement);
+	replace_file << text;
 
 	original_file.close();
 	replace_file.close();
