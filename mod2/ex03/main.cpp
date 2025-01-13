3af3022594ec005c7c4654ed949f552b2ebab341
Anttoni Maula <manttoni92@gmail.com>
2025-01-13 21:22:06 +0200

    compiles

--- mod2/ex03/main.cpp
+++ mod2/ex03/main.cpp
@@ -1,4 +1,4 @@
-#include "Point.cpp"
+#include "Point.hpp"
 #include <iostream>
 
 int main(void)
@@ -7,6 +7,6 @@ int main(void)
 	Point b = Point(10, 10);
 	Point c = Point(10, 0);
 
-	std::cout << bsp(a, b, c, Point(2, 1)) << std::endl;
+	std::cout << "True or no: " << bsp(a, b, c, Point(2, 1)) << std::endl;
 	return 0;
 }
