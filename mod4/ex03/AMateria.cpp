a113bf43263594f95f6b3e72bf8800b9510682db
Anttoni Maula <amaula@c1r5p20.hive.fi>
2025-01-20 19:02:54 +0200

    asd

--- mod4/ex03/AMateria.cpp
+++ mod4/ex03/AMateria.cpp
@@ -1,15 +1,11 @@
 #include "AMateria.hpp"
 
-// default constructor
 AMateria::AMateria() : type("") {}
 
-// constructor with type
 AMateria::AMateria(std::string const & type) : type(type) {}
 
-// copy constructor
 AMateria::AMateria(const AMateria& other) : type(other.getType()) {}
 
-// copy assignment
 AMateria& AMateria::operator=(const AMateria& other)
 {
     if (this != &other)
@@ -17,8 +13,6 @@ AMateria& AMateria::operator=(const AMateria& other)
     return *this;
 }
 
-// getters
 std::string const & AMateria::getType() const { return type; }
 
-// destructor
 AMateria::~AMateria() {}
