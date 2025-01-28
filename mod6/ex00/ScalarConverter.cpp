20a7ac6ed8ffe1fb7605e2faed944e20e5b2c522
Anttoni Maula <manttoni92@gmail.com>
2025-01-28 20:58:05 +0200

    ScalarConverter

--- mod6/ex00/ScalarConverter.cpp
+++ mod6/ex00/ScalarConverter.cpp
@@ -1,36 +1,69 @@
 #include "ScalarConverter.hpp"
 #include <string>
 #include <iostream>
-#include <cmath>
+#include <limits>
+#include <climits>
 #include <iomanip>
+#include <cmath>
+#include <cfloat>
 
-static type getType(std::string s)
+static bool isType(const std::string& s)
 {
-    if (s[0] == '-') s.erase(0,1);
-    if (s.empty()) return NONE;
+    size_t len = s.size();
+    if (len == 1)
+        return true;
+    if (len == 0)
+        throw std::runtime_error("input is empty");
+
+    if (s.find_first_of("f") < s.size() - 1)
+        throw std::runtime_error("\'f\' is not last");
+
+    if (s.find_first_of("-") != std::string::npos && s.find_last_of("-") > 0)
+        throw std::runtime_error("\'-\' is not first");
+
+    if (s.find_first_of(".") != s.find_last_of("."))
+        throw std::runtime_error("multiple \'.\'");
+
+    if (s.find_first_not_of("f-.1234567890") != std::string::npos)  
+        throw std::runtime_error("forbidden character");
+
+    if (s.find_first_of("1234567890") == std::string::npos)
+        throw std::runtime_error("no number");
+
+    return true;
+}
+
+static bool isFloat(const std::string& s)
+{
+    return s.back() == 'f';
+}
+
+static bool isInt(const std::string& s)
+{
+    try                         { return stod(s) == (double)stoi(s); }
+    catch(std::exception& e)    { return false; }
+}
+
+static bool isChar(const std::string& s)
+{
+    if (s.size() > 1)
+        return false;
+    if (s[0] >= '0' && s[0] <= '9')
+        return false;
+    return true;
+}
+
+static type getType(const std::string& s)
+{
     if (s == "-inff" || s == "+inff" || s == "nanf")
         return FLOAT;
     else if (s == "-inf" || s == "+inf" || s == "nan")
         return DOUBLE;
 
-    int periods = 0;
-    for (size_t i = 0; i < s.length(); ++i)
-    {
-        if (s.length() == 1) break;
-        if (s[i] == 'f' && i < s.length() - 1)
-            return NONE;
-        if (s[i] == '.' && periods != 0)
-            return NONE;
-        if ((s[i] > '9' || s[i] < '0') && s[i] != '.' && s[i] != 'f')
-            return NONE;
-    }
-
-    if (s.length() == 1 && (s[0] > '9' || s[0] < '0'))
-        return CHAR;
-    if (s.back() == 'f')
-        return FLOAT;
-    if (s.find('.') == std::string::npos)
-        return INT;
+    if (isType(s) == false) return NONE;
+    if (isChar(s) == true)  return CHAR;
+    if (isFloat(s) == true) return FLOAT;
+    if (isInt(s) == true)   return INT;
     return DOUBLE;
 }
 
@@ -48,7 +81,6 @@ static size_t getPrecision(const std::string& s)
 void ScalarConverter::convert(const std::string& s)
 {
     type t = getType(s);
-    int precision = 1;
 
     char    c;
     int     i;
@@ -58,34 +90,36 @@ void ScalarConverter::convert(const std::string& s)
     switch (t)
     {
         case CHAR:
-            //std::cout << "CHAR" << std::endl;
+            std::cout << "CHAR" << std::endl;
             c = static_cast<char>(s[0]);
             i = static_cast<int>(c);
             f = static_cast<float>(c);
             d = static_cast<double>(c);
             break;
         case INT:
-            //std::cout << "INT" << std::endl;
+            std::cout << "INT" << std::endl;
             i = stoi(s);
             c = static_cast<char>(i);
             f = static_cast<float>(i);
             d = static_cast<double>(i);
             break;
         case FLOAT:
-            //std::cout << "FLOAT" << std::endl;
-            f = stof(s);
+            std::cout << "FLOAT" << std::endl;
+            try { f = stof(s); }
+            catch(std::exception& e) {
+                f = std::numeric_limits<float>::infinity();
+                if (s[0] == '-') f *= -1;
+            }
             c = static_cast<char>(f);
             i = static_cast<int>(f);
             d = static_cast<double>(f);
-            precision = getPrecision(s);
             break;
         case DOUBLE:
-            //std::cout << "DOUBLE" << std::endl;
+            std::cout << "DOUBLE" << std::endl;
             d = stod(s);
             c = static_cast<char>(d);
             i = static_cast<int>(d);
             f = static_cast<float>(d);
-            precision = getPrecision(s);
             break;
         default:
             throw std::runtime_error("Not any supported type");
@@ -93,14 +127,18 @@ void ScalarConverter::convert(const std::string& s)
 
     std::cout << "char:   ";
     if (i < 0 || i > 127)       std::cout << "impossible" << std::endl;
-    else if (i < 32 || i > 126) std::cout << "Non printable" << std::endl;
+    else if (i < 32 || i > 126) std::cout << "Non displayable" << std::endl;
     else                        std::cout << c << std::endl;
 
     std::cout << "int:    ";
-    if (std::isnan(d) || std::isinf(d)) std::cout << "impossible" << std::endl;
-    else                                std::cout << i << std::endl;
+    if (std::isnan(d) || std::isinf(d))            std::cout << "impossible" << std::endl;
+    else if (d - 1 >= INT_MAX || d + 1 <= INT_MIN) std::cout << "overflow" << std::endl;
+    else                                           std::cout << i << std::endl;
 
-    std::cout << std::fixed << std::setprecision(precision);
+
+    std::cout << std::fixed << std::setprecision(getPrecision(s));
+    
     std::cout << "float:  " << f << "f" << std::endl;
+    
     std::cout << "double: " << d << std::endl;
 }
--- mod6/ex00/main.cpp
+++ mod6/ex00/main.cpp
@@ -3,70 +3,54 @@
 #include <string>
 #include <exception>
 
-static bool tryConversion(const std::string& s)
+const std::string tests[] = { "2147483648", "-2147483649", "2147483647", "-2147483648",
+                        "99999999999999999999", "999999999999999999999999999999",
+                        "9999999999999999999999999999999999999999",
+                        "1", "11", "111", "1111",
+                        "1.", "11.", "111.", "1111.",
+                        "1.1", "11.11", "111.111", "1111.1111",
+                        ".1", ".11", ".111", ".1111",
+                        "+inf", "-inf", "nan",
+                        "asd", "qwe", "zxc",
+                        ".", "-", "f", "",
+                        "1a", "1.1a", "1.a", ".a1", " " };
+
+static void tryConversion(const std::string& s)
 {
-    try {
-        ScalarConverter::convert(std::string(s));
-        return true;
-    }
-    catch(std::exception& e) { 
-        std::cerr << e.what() << std::endl;
-        return false;
+    try                         { ScalarConverter::convert(s); }
+    catch(std::exception& e)    { std::cerr << "Error: " << e.what() << std::endl; }
+}
+
+static void testRun(const std::string& pre, const std::string& post)
+{
+    for (std::string s : tests)
+    {
+        std::string input = pre + s + post;
+        std::cout << "~~~~:" << input << ":~~~~" << std::endl;
+        tryConversion(input);
+        std::getline(std::cin, input);
+        if (input != "") break;
     }
 }
 
-int main(void)
+int main(int ac, char **av)
 {
-    /*
-    if (ac != 2) {
-        std::cout << "Usage: " << av[0] << " <str to convert>" << std::endl;
-        return 1;
-    }
-    */
-
-    std::string asd;
-
-    std::string tests[] = { "12345678901234567890",
-                            "1", "11", "111", "1111",
-                            "1.", "11.", "111.", "1111.",
-                            "1.1", "11.11", "111.111", "1111.1111",
-                            "+inf", "-inf", "nan",
-                            "asd", "qwe", "zxc",
-                            "1234567890", "",
-                            "a", "aa", "aaa", "aaaa",
-                            ".1", ".11", ".111", ".1111" };
+    for (int i = 1; i < ac; ++i)
+        tryConversion(av[i]);
+    if (ac != 1)
+        return (0);
 
     std::cout << "====== NO MODIFICATIONS ======" << std::endl;
-    for (std::string s : tests)
-    {
-        std::cout << "~~~~: " << s << " :~~~~" << std::endl;
-        tryConversion(s);
-        std::getline(std::cin, asd);
-    }
+    testRun("", "");
 
-    std::cout << "====== AS FLOAT ======" << std::endl;
-    for (std::string s : tests)
-    {
-        std::cout << "~~~~: " << s + "f" << " :~~~~" << std::endl;
-        tryConversion(s + "f");
-        std::getline(std::cin, asd);
-    }
+    std::cout << "====== FLOAT ======" << std::endl;
+    testRun("", "f");
 
     std::cout << "====== NEGATIVE ======" << std::endl;
-    for (std::string s : tests)
-    {
-        std::cout << "~~~~: " << "-" + s << " :~~~~" << std::endl;
-        tryConversion("-" + s);
-        std::getline(std::cin, asd);
-    }
+    testRun("-", "");
 
-    std::cout << "====== BOTH MODS ======" << std::endl;
-    for (std::string s : tests)
-    {
-        std::cout << "~~~~: " << "-" + s << " :~~~~" << std::endl;
-        tryConversion("-" + s);
-        std::getline(std::cin, asd);
-    }
+    std::cout << "====== NEGATIVE FLOAT ======" << std::endl;
+    testRun("-", "f");
 
     return 0;
 }
